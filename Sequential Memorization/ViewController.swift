7f1ed73e1fdfd79472a8ecf3708f1e3fc1650ed7
Kimjeongguk <mindstar7949@naver.com>
2021-02-25 13:05:37 +0900

    Initial Commit

---
+++ Sequential Memorization/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sequential Memorization
+//
+//  Created by Jeongguk Kim on 2021/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
